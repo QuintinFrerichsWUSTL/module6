853f0ede03addecfd58249bd63de43db623f6128
Eric Goodman <ericgoodman37@gmail.com>
2018-03-28 03:52:20 -0500

    almost functional

---
+++ server.js
@@ -0,0 +1,50 @@
+/**
+ * Created by ericgoodman on 3/26/18.
+ */
+// Require the packages we will use:
+var http = require("http"),
+    socketio = require("socket.io"),
+    fs = require("fs");
+
+// Listen for HTTP connections.  This is essentially a miniature static file server that only serves our one file, client.html:
+var app = http.createServer(function(req, resp){
+    // This callback runs when a new connection is made to our HTTP server.
+
+    fs.readFile("client.html", function(err, data){
+        // This callback runs when the client.html file has been read from the filesystem.
+        if(err) return resp.writeHead(500);
+        resp.writeHead(200);
+        resp.end(data);
+    });
+});
+app.listen(3456);
+
+var rooms = [];
+
+// Do the Socket.IO magic:
+var io = socketio.listen(app);
+io.sockets.on("connection", function(socket){
+    // This callback runs when a new Socket.IO connection is established.
+
+    socket.on('message_to_server', function(data) {
+        // This callback runs when the server receives a new message from the client.
+        console.log(data); // log it to the Node.JS output
+        io.sockets.emit("message_to_client", data) // broadcast the message to other users
+    });
+
+    socket.on('new_room', function(room) {
+        rooms.push(room);
+        console.log(room);
+    });
+
+    socket.on('delete_room', function(room) {
+        for(var i = 0; i < rooms.length; i++) {
+            if (rooms[i].name === room.name) {
+                rooms.remove(i);
+                return
+            }
+        }
+    });
+
+
+});
